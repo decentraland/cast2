95baa60d532b56e083d9325574b50f281519c0b7
Gabriel Diaz <gabrieldiaz31@gmail.com>
2026-04-06 10:49:29 -0300

    chore: Restrict file upload to PDF only for testing

--- src/components/SharePresentationModal/SharePresentationModal.tsx
+++ src/components/SharePresentationModal/SharePresentationModal.tsx
@@ -77,9 +77,9 @@ export function SharePresentationModal({ onClose, onFileSelected, onUrlSubmitted
         <Divider>or</Divider>
 
         <BrowseButton onClick={handleBrowse}>Browse your local files</BrowseButton>
-        <input ref={fileInputRef} type="file" accept=".pdf,.pptx" style={{ display: 'none' }} onChange={handleFileChange} />
+        <input ref={fileInputRef} type="file" accept=".pdf" style={{ display: 'none' }} onChange={handleFileChange} />
 
-        <SupportedFormats>Supported formats: gslides (Google Slides) and PDF.</SupportedFormats>
+        <SupportedFormats>Supported format: PDF.</SupportedFormats>
       </Modal>
     </Overlay>
   )
