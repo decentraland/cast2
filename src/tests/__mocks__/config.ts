f26487a722cffa7d904340a5981cd5b99f9459bb
Gabriel Diaz <gabrieldiaz31@gmail.com>
2026-04-21 18:32:54 -0300

    fix: typo

--- src/tests/__mocks__/config.ts
+++ src/tests/__mocks__/config.ts
@@ -1,7 +1,7 @@
 export const config = {
   get: (key: string, defaultValue?: string) => {
     const configs: Record<string, string> = {
-      LIVEKIT_URL: 'wss://test-livekit.example.co',
+      LIVEKIT_URL: 'wss://test-livekit.example.com',
       API_URL: 'http://localhost:3000'
     }
     return configs[key] || defaultValue || ''
