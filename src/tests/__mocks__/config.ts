c73b33a44b58fdc90a773f60dd82533881c10789
Gabriel Diaz <gabrieldiaz31@gmail.com>
2026-04-21 14:50:36 -0300

    chore: Update mock

--- src/tests/__mocks__/config.ts
+++ src/tests/__mocks__/config.ts
@@ -1,7 +1,7 @@
 export const config = {
   get: (key: string, defaultValue?: string) => {
     const configs: Record<string, string> = {
-      LIVEKIT_URL: 'wss://test-livekit.example.com',
+      LIVEKIT_URL: 'wss://test-livekit.example.co',
       API_URL: 'http://localhost:3000'
     }
     return configs[key] || defaultValue || ''
